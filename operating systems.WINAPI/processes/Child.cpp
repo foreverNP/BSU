2af2235245542662e116086e27ded3bca96765c2
foreverNP <levsegeenko@gmail.com>
2024-02-28 18:35:21 +0300

    add processes

---
+++ operating systems.WINAPI/processes/Child.cpp
@@ -0,0 +1,47 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <conio.h>
+#include <sstream>
+#include <Windows.h>
+
+using namespace std;
+
+bool isDigit(char c)
+{
+    return c >= '0' && c <= '9';
+}
+
+int main(int argc, char *argv[])
+{
+    for (int i = 0; i < argc; i++)
+    {
+        cout << argv[i] << " ";
+    }
+
+    if (argc <= 1)
+    {
+        std::cout << "No arguments provided." << std::endl;
+        return 0;
+    }
+
+    cout << "\nNumber of elements: " << argc - 1 << endl;
+
+    int size = argc - 1;
+    char *values = new char[size];
+
+    cout << "Only digits:\n";
+    for (int i = 0; i < size; i++)
+    {
+        if (isDigit(*argv[i + 1]))
+        {
+            cout << *argv[i + 1] << " ";
+        }
+    }
+    cout << "\n";
+
+    _getch();
+
+    delete[] values;
+
+    return 0;
+}
