04aa06da4b9f2a7a7b1752452b22cddff22c61de
foreverNP <levsegeenko@gmail.com>
2024-04-22 20:45:51 +0300

    add os lab 4,5

---
+++ operating systems.WINAPI/5. anonymous channel/Alfavit.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <vector>
+#include <cctype>
+
+using namespace std;
+
+bool isLatin(char c)
+{
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
+
+int main(int argc, char *argv[])
+{
+    HANDLE hWritePipe = (HANDLE)atoi(argv[1]), hReadPipe = (HANDLE)atoi(argv[2]);
+
+    int size;
+    DWORD dwBytesRead;
+    ReadFile(hReadPipe, &size, sizeof(int), &dwBytesRead, NULL);
+
+    cout << "Size of array: " << size << endl;
+    vector<char> arr(size);
+    cout << "Received array" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        ReadFile(hReadPipe, &arr[i], sizeof(char), &dwBytesRead, NULL);
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Latin letters array" << endl;
+    vector<char> latinLetters;
+    for (int i = 0; i < size; ++i)
+    {
+        if (isLatin(arr[i]))
+        {
+            latinLetters.push_back(arr[i]);
+            cout << arr[i] << " ";
+        }
+    }
+    cout << endl;
+
+    int latinLettersCount = latinLetters.size();
+    WriteFile(hWritePipe, &latinLettersCount, sizeof(int), &dwBytesRead, NULL);
+
+    for (int i = 0; i < latinLettersCount; ++i)
+    {
+        WriteFile(hWritePipe, &latinLetters[i], sizeof(char), &dwBytesRead, NULL);
+    }
+
+    CloseHandle(hWritePipe);
+    CloseHandle(hReadPipe);
+
+    int a;
+    cin >> a;
+
+    return 0;
+}
---
+++ operating systems.WINAPI/5. anonymous channel/Server.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int sizeOfArray;
+    cout << "Input size of array: ";
+    cin >> sizeOfArray;
+
+    vector<char> array(sizeOfArray);
+    cout << "Input elements of array: ";
+    for (int i = 0; i < sizeOfArray; ++i)
+    {
+        cin >> array[i];
+    }
+
+    HANDLE hWritePipe1, hReadPipe1;
+    HANDLE hWritePipe2, hReadPipe2;
+
+    SECURITY_ATTRIBUTES sa;
+    sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+    sa.lpSecurityDescriptor = NULL;
+    sa.bInheritHandle = TRUE;
+
+    CreatePipe(&hReadPipe1, &hWritePipe1, &sa, 0);
+    CreatePipe(&hReadPipe2, &hWritePipe2, &sa, 0);
+
+    STARTUPINFO stp;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&stp, sizeof(STARTUPINFO));
+    stp.cb = sizeof(STARTUPINFO);
+    wchar_t commandLine[80];
+    wsprintf(commandLine, L"Alfavit.exe %d %d", (int)hWritePipe2, (int)hReadPipe1);
+    CreateProcess(NULL, commandLine, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &stp, &pi);
+
+    DWORD dwBytesWritten;
+    WriteFile(hWritePipe1, &sizeOfArray, sizeof(int), &dwBytesWritten, NULL);
+    for (int i = 0; i < sizeOfArray; i++)
+    {
+        WriteFile(hWritePipe1, &array[i], sizeof(char), &dwBytesWritten, NULL);
+    }
+
+    CloseHandle(hWritePipe1);
+    WaitForSingleObject(pi.hProcess, INFINITE);
+
+    DWORD dwBytesRead;
+    int sizeOfNewArray;
+    ReadFile(hReadPipe2, &sizeOfNewArray, sizeof(int), &dwBytesRead, NULL);
+    vector<char> resultArray(sizeOfNewArray);
+
+    cout << "Received Latin Letters from Alfavit Process: ";
+    for (int i = 0; i < sizeOfNewArray; ++i)
+    {
+        ReadFile(hReadPipe2, &resultArray[i], sizeof(char), &dwBytesRead, NULL);
+        cout << resultArray[i] << " ";
+    }
+    cout << endl;
+
+    CloseHandle(hReadPipe2);
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    CloseHandle(hWritePipe2);
+    CloseHandle(hReadPipe1);
+
+    return 0;
+}
