fe1862b55faaafad95b549e472ffa15d0a30952d
lev <levsergienkowork@gmail.com>
2025-03-09 13:34:03 +0300

    add MT

---
+++ 3.2/MT/MTK/slnTwoModules/TwoModules/Mod2.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+
+
+extern int scalar(int n, float v[], float w[], float* vw);
+
+int scalar(int n, float v[], float w[], float* vw)
+{
+	if (n != 3) return 1;
+	*vw = 0;
+	for (int i = 0; i < n; i++)
+		*vw = *vw + v[i] * w[i];
+	return 0;
+
+}
